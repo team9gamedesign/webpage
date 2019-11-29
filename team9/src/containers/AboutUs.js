89bf30c8181fab746541d78a619c8173a6fc9ccc
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-11-29 11:01:18 +0100

    Added background image

--- team9/src/containers/AboutUs.js
+++ team9/src/containers/AboutUs.js
@@ -9,22 +9,22 @@ class AboutUs extends Component {
             <Card title='About us'>
                 <div className='aboutUsContent'>
                     <div className='aboutUsPerson'>
-                        <img className='aboutUsImage' src={fredrik} />
+                        <img className='aboutUsImage' src={fredrik} alt='' />
                         <div className='aboutUsName'>{'CHANTAL HUNDSCHEID'}</div>
                         <div className='aboutUsText'></div>
                     </div>
                     <div className='aboutUsPerson'>
-                        <img className='aboutUsImage' src={fredrik} />
+                        <img className='aboutUsImage' src={fredrik} alt='' />
                         <div className='aboutUsName'>{'FREDRIK OMSTEDT'}</div>
                         <div className='aboutUsText'>I am studying my final year of the Computer Science Master's programme at KTH. When I'm not playing games, I like to play golf and the guitar. In this project, my focus has been on designing and implementing the classes.</div>
                     </div>
                     <div className='aboutUsPerson'>
-                        <img className='aboutUsImage' src={fredrik} />
+                        <img className='aboutUsImage' src={fredrik} alt='' />
                         <div className='aboutUsName'>{'MARCUS WERLINDER'}</div>
                         <div className='aboutUsText'></div>
                     </div>
                     <div className='aboutUsPerson'>
-                        <img className='aboutUsImage' src={fredrik} />
+                        <img className='aboutUsImage' src={fredrik} alt='' />
                         <div className='aboutUsName'>{'MARKUS WINROTH'}</div>
                         <div className='aboutUsText'></div>
                     </div>
